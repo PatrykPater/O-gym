37001bab4af532f15c0e3fce0442d31eae6945e6
Patryk Pater <p.pater@sprinter.com.pl>
2021-12-14 20:08:42 +0100

    added common project for tests, initial work on membership feature at application level and its tests

---
+++ O-gym.Application.Tests/FeaturesTests/UserMembershipTests/CommandsTests/AddMembershipCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using O_gym.Application.Features.UserMembership.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace O_gym.Application.Tests.FeaturesTests.UserMembershipTests.CommandsTests
+{
+    public class AddMembershipCommandTests
+    {
+        private readonly AddMembershipHandler _commandHandler;
+        private readonly Mock<IUserRepository> repository = new();
+
+        public AddMembershipCommandTests()
+        {
+            _commandHandler = new AddMembershipHandler(repository.Object);
+        }
+
+        private Task Act(AddMembershipCommand command, CancellationToken cancellation)
+            => _commandHandler.Handle(command, cancellation);
+
+        [Fact]
+        public async Task HandleAsync_Should_CallRepository_WithSuccess()
+        {
+            var command = new AddMembershipCommand();
+
+            CancellationTokenSource source = new();
+            CancellationToken token = source.Token;
+
+            var exception = await Record.ExceptionAsync(() => Act(command, token));
+
+            Assert.Null(exception);
+            await repository.Verify(r => r.Update);
+        }
+    }
+}
---
+++ O-gym.Application/Features/UserMembership/Commands/AddMembershipCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace O_gym.Application.Features.UserMembership.Commands
+{
+    public class AddMembershipCommand: IRequest<Guid>
+    {
+    }
+
+    public class AddMembershipHandler : IRequestHandler<AddMembershipCommand, Guid>
+    {
+        public Task<Guid> Handle(AddMembershipCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
